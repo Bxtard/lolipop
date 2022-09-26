f2a7a77461be133371e4a26b7703a66258c30cc8
Bryan <ee.mxtrd@gmail.com>
2022-09-26 14:28:50 -0500

    fixes

--- pages/api/video/index.ts
+++ pages/api/video/index.ts
@@ -23,15 +23,6 @@ export default async function handler(
     case 'GET':
       const videos = await prisma.video.findMany({});
       return res.json(videos);
-    case 'POST':
-    /* try {
-        const response = await prisma.video.create({
-          data: {videoUrl, name, image },
-        });
-        return res.status(200).json(response);
-      } catch (error) {
-        return res.status(500).json({ error });
-      } */
     default:
       return res.status(405).json({ message: 'Invalid method' });
   }
